1d334aa90a63329cac7dddb2f201be19847dd0c2
Doney den Ouden <doney@doney.nl>
2019-04-23 17:27:44 +0200

    Improvements

--- ios/RNSplashScreen.h
+++ ios/RNSplashScreen.h
@@ -1,16 +1,10 @@
-/**
- * SplashScreen
- * 启动屏
- * from：http://www.devio.org
- * Author:CrazyCodeBoy
- * GitHub:https://github.com/crazycodeboy
- * Email:crazycodeboy@gmail.com
- */
 #import <React/RCTBridgeModule.h>
 #import <UIKit/UIKit.h>
 
 @interface RNSplashScreen : NSObject<RCTBridgeModule>
-+ (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView;
-+ (void)show;
-+ (void)hide;
+@property(nonatomic, strong) UIViewController *launchScreen;
+- (void)showSplash:(NSString*)splashScreen inRootView:(UIView*)rootView;
+- (void)show;
+- (void)hide;
+- (void)setup;
 @end
